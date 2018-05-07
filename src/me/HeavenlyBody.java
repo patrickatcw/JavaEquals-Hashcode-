4b49b23ea7c274cac730b2bb1b9bd0c9b8574c60
patrickatcw <pj8392@yahoo.com>
2018-05-07 10:27:29 -0400

    override method and override hashcode, back to main next to test

--- src/me/HeavenlyBody.java
+++ src/me/HeavenlyBody.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 //step 1 class
 //step 2 make final class
+@SuppressWarnings("ALL")
 public final class HeavenlyBody {
 
     //step 3 fields or variables
@@ -40,6 +41,37 @@ public final class HeavenlyBody {
         return this.satellites.add(moon);
 
         //step 9 to main
+
+    }
+
+    //step 4 this program, override method
+    @Override
+    //public boolean equals(HeavenlyBody obj){   //done to get rid of error on override
+    public boolean equals(Object obj) {
+        if (this==obj) {    //first check to see if equal to itself
+            return true;    //and if = to itsself it will return true, if object
+        }                   //you are comparing is same object in memory
+
+        System.out.println("obj.getClass() is " + obj.getClass());  //use to see when our equals method is called
+        System.out.println("this.getClass() is " + this.getClass()); //as well as what it is doing
+        if ((obj==null) || (obj.getClass())!=this.getClass()) { //test for null first,
+            return false;
+        }
+
+        //another test
+        String objName = ((HeavenlyBody) obj).getName();
+        return this.name.equals(objName);
+
+    }
+
+    //step 5 this program, implement our hashcode, override hashcode
+    @Override
+    public int hashCode() {
+        System.out.println("hashcode called"); //added this for our benifit to see called
+        //return super.hashCode();, changed to.....
+        return this.name.hashCode() + 57;  //number garantees we have a non-zero number for each of our hashes
+
+        //step 6 this program to main to run
 
     }
 
